8236655e5ec7e5cee928c33eb839837166defd5f
Eyob Eshetu <eyob2901@gmail.com>
2022-10-13 12:56:12 -0700

    cocktail

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,106 @@
+#include "sort.h"
+
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
+
+/**
+* swap_node_ahead - function that swaps a node in a doubly-linked listint_t
+* list of integers with the node ahead of it
+* @list: double head pointer of a doubly-linked list of integers
+* @tail: double tail pointer of the doubly-linked list
+* @shaker: double pointer to the current swapping node of the cocktail
+* shaker algorithm
+*
+* Return: nothing
+*/
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
+{
+	listint_t *temp = (*shaker)->next;
+
+	if ((*shaker)->prev != NULL)
+		(*shaker)->prev->next = temp;
+	else
+		*list = temp;
+	temp->prev = (*shaker)->prev;
+	(*shaker)->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = *shaker;
+	else
+		*tail = *shaker;
+	(*shaker)->prev = temp;
+	temp->next = *shaker;
+	*shaker = temp;
+}
+
+/**
+* swap_node_behind - function that swaps a node in a doubly-linked listint_t
+* list of integers with the node behind it
+* @list: double head pointer to the doubly-linked list of integers
+* @tail:double tail pointer to the doubly-linked list
+* @shaker: double pointer to the current swapping node of the cocktail
+* shaker algorithm
+*
+* Return: nothing
+*/
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
+{
+	listint_t *temp = (*shaker)->prev;
+
+	if ((*shaker)->next != NULL)
+		(*shaker)->next->prev = temp;
+	else
+		*tail = temp;
+	temp->next = (*shaker)->next;
+	(*shaker)->prev = temp->prev;
+	if (temp->prev != NULL)
+		temp->prev->next = *shaker;
+	else
+		*list = *shaker;
+	(*shaker)->next = temp;
+	temp->prev = *shaker;
+	*shaker = temp;
+}
+
+/**
+* cocktail_sort_list - function that sorts a doubly linked list of integers
+* in ascending order using the Cocktail shaker sort algorithm
+* @list: double head pointer to the doubly-linked listint_t list
+*
+* Return: nothing
+*/
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *tail, *shaker;
+	bool shaken_not_stirred = false;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (tail = *list; tail->next != NULL;)
+		tail = tail->next;
+
+	while (shaken_not_stirred == false)
+	{
+		shaken_not_stirred = true;
+		for (shaker = *list; shaker != tail; shaker = shaker->next)
+		{
+			if (shaker->n > shaker->next->n)
+			{
+				swap_node_ahead(list, &tail, &shaker);
+				print_list((const listint_t *)*list);
+				shaken_not_stirred = false;
+			}
+		}
+		for (shaker = shaker->prev; shaker != *list;
+				shaker = shaker->prev)
+		{
+			if (shaker->n < shaker->prev->n)
+			{
+				swap_node_behind(list, &tail, &shaker);
+				print_list((const listint_t *)*list);
+				shaken_not_stirred = false;
+			}
+		}
+	}
+}
